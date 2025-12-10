7c9736934b56ce8401e38287c11ccd02036e5a73
TurnerHelical <hunterde91@gmail.com>
2025-12-10 10:42:51 -0500

    created auth and passport config

---
+++ src/auth/localStrategy.js
@@ -0,0 +1,22 @@
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcrypt';
+import { prisma } from '../lib/prisma.js';
+
+export const localStrategy = new LocalStrategy(
+    { usernameField: 'email' },
+    async (email, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { email } });
+            if (!user) {
+                return done(null, false, { message: 'Invalid credentials' })
+            };
+            const match = await bcrypt.compare(password, user.passwordHash);
+            if (!match) {
+                return done(null, false, { message: 'Invalid credentials' })
+            };
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+);
